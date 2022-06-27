ee6780d6a7d781c7d444eb4e9be14b32de0a5beb
keshavsa <keshavsapkota2018@gmail.com>
2022-06-27 19:38:53 +0545

    this is my first commit

---
+++ do-while.java
@@ -0,0 +1,23 @@
+import java.util.Scanner
+public class do-while {
+
+    public static void main(String[] args) {
+       int i=1,n,b,c,d;
+        Scanner myObj =new Scanner(System.in);
+        System.out.println("Enter the username");
+        int n=myObj.nextInt();
+        b=0;
+        c=1;
+        d=1;
+        do{
+           i++;
+           System.out.println(b);
+           c=d;
+           d=b;
+           b=d+c;
+            
+        }
+        while(n>=i)
+        
+    }
+}
